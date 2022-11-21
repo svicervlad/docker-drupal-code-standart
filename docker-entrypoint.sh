e089a5228f1382fcc44348d74e83e36e3ba5ad47
svicervlad <vlad.sadretdinov@jet.dev>
2022-11-21 13:28:22 +0000

    fix: remove css

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,6 @@ export PATH="$PATH:/root/.composer/vendor/bin"
 
 phpcs \
   --standard=Drupal,DrupalPractice \
-  --extensions=php,module,inc,install,test,profile,theme,css,info,txt,md,yml \
+  --extensions=php,module,inc,install,test,profile,theme,info,txt,md,yml \
   --report=junit "$@"
 
